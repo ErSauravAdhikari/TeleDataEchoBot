13155539301b30431e2972618e8371b0f50c56c0
Saurav Adhikari <ErSauravAdhikari@gmail.com>
2021-08-17 14:57:45 +0545

    Initial Code Completed

---
+++ README.md
@@ -0,0 +1,22 @@
+# TeleDataEchoBot
+
+It is a simple telegram bot that returns the data recieved from user.
+
+## Commands
+- /userid Reurns user's effective user id
+- /help Returns help message
+- /start Returns start message
+- /json Echo backs complete JSON that is recieved
+
+## Usage
+Fork this repo and then deploy it to Heroku.
+
+You will need to add following environment variable
+- TELEDATAECHOBOT_API_TOKEN
+
+Which is your API Token
+
+## Hosted Version
+You can use the hosted version of this bot at:
+
+https://t.me/teledataechobot
---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+
+
+
+def get_user_id(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(f'Your User ID is: {update.effective_user.id}')
+
+
+def get_full_json(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(f'{update}')
+
+
+def get_start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(f"""
+/userid Reurns user's effective user id
+/help [/start] Returns this message
+/json Echo backs complete JSON that is recieved
+
+Hello, I am Saurav Adhikari. The Creator of this BOT.
+How about you give me some stars and follows at github, it would definately help my vanity. 
+    
+https://github.com/ersauravadhikari/
+
+🤣🤣🤣
+    """)
+
+
+def main():
+    TELEGRAM_API = os.environ.get("TELEDATAECHOBOT_API_TOKEN")
+    updater = Updater(TELEGRAM_API)
+    updater.dispatcher.add_handler(CommandHandler('userid', get_user_id))
+    updater.dispatcher.add_handler(CommandHandler('json', get_full_json))
+    updater.dispatcher.add_handler(CommandHandler('start', get_start))
+    updater.dispatcher.add_handler(CommandHandler('help', get_start))
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == "__main__":
+    main()
